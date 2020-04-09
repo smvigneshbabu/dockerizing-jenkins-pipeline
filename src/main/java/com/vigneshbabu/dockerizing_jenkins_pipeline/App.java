bc2751e188f2c1439f8e32ac2b42e59f3f8bfa74
Ubuntu <vigneshbabusury@vigneshbabusury.iarphi1apxnejkhlxth3fkztod.gx.internal.cloudapp.net>
2020-04-09 17:35:46 +0000

    commiting for the first time

---
+++ src/main/java/com/vigneshbabu/dockerizing_jenkins_pipeline/App.java
@@ -0,0 +1,13 @@
+package com.vigneshbabu.dockerizing_jenkins_pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
